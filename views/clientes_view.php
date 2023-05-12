af1dc2a0a648bdd9c6ce22d2361368c1d029f009
JuanPe1981 <juanpe981@me.com>
2023-05-12 17:46:16 +0200

    Aplicación de php que muestra un listado de clientes y prendas de ropa de la API mejora diseño

--- views/clientes_view.php
+++ views/clientes_view.php
@@ -6,10 +6,12 @@
         <p class="card-text"><?php echo $cliente['id_cliente']?></p>
     </div>
     <div class="card-body">
-        <h5 class="card-title"><?php echo $cliente['nombre']?></h5>
-        <p class="card-text"><?php echo $cliente['fecha_alta']?></p>
-        <p class="card-text"><?php echo $cliente['club_vip']?></p>
-        <p class="card-text"><?php echo $cliente['numero_pedidos']?></p>
+        <h5 class="card-title">Nombre: <?php echo $cliente['nombre']?></h5>
+        <p class="card-text">Fecha de Alta: <?php echo $cliente['fecha_alta']?></p>
+        <p class="card-text">Club Vip:
+            <?php  echo $cliente['club_vip'] ? 'Yes' : 'No'?>
+        </p>
+        <p class="card-text">Numero de pedidos: <?php echo $cliente['numero_pedidos']?></p>
 <!--        <a href="--><?php //echo URLSITE?><!--/?page=vermensajes&idtema=--><?php //echo $cliente['idcliente']?><!--" class="btn btn-primary">IR A TEMA</a>-->
     </div>
     <?php endforeach; ?>
--- views/ropas_view.php
+++ views/ropas_view.php
@@ -6,11 +6,11 @@
         <p class="card-text"><?php echo $ropa['id_ropa']?></p>
     </div>
     <div class="card-body">
-        <h5 class="card-title"><?php echo $ropa['nombre']?></h5>
-        <p class="card-text"><?php echo $ropa['code']?></p>
-        <p class="card-text"><?php echo $ropa['fecha_alta']?></p>
-        <p class="card-text"><?php echo $ropa['hay_stock']?></p>
-        <p class="card-text"><?php echo $ropa['precio']?></p>
+        <h5 class="card-title">Nombre: <?php echo $ropa['nombre']?></h5>
+        <p class="card-text">Code: <?php echo $ropa['code']?></p>
+        <p class="card-text">Fecha de Alta: <?php echo $ropa['fecha_alta']?></p>
+        <p class="card-text">Hay Stock: <?php echo $ropa['hay_stock'] ? 'Yes' : 'No'?></p>
+        <p class="card-text">Precio: <?php echo $ropa['precio']?></p>
 <!--        <a href="--><?php //echo URLSITE?><!--/?page=vermensajes&idtema=--><?php //echo $cliente['idcliente']?><!--" class="btn btn-primary">IR A TEMA</a>-->
     </div>
     <?php endforeach; ?>
